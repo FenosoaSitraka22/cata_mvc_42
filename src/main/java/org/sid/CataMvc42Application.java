a1c7ed4443f735c68b4f3590c1526c2318dcd011
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2020-06-12 13:42:12 +0200

    postgresql

---
+++ src/main/java/org/sid/CataMvc42Application.java
@@ -0,0 +1,24 @@
+package org.sid;
+
+import org.sid.dao.ProduitRepository;
+import org.sid.entities.Produit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CataMvc42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CataMvc42Application.class, args);
+		ApplicationContext ac = SpringApplication.run(CataMvc42Application.class, args);
+		ProduitRepository pr = ac.getBean(ProduitRepository.class);
+		for (int i = 0; i < 30; i++) {
+			String s =String.format("%d", i);
+			Double prix = (double) (i*10);
+			Produit p = new Produit(s,prix,i);
+			pr.save(p);
+		}
+	}
+
+}
---
+++ src/main/java/org/sid/web/ProduitController.java
@@ -0,0 +1,43 @@
+package org.sid.web;
+
+import java.util.List;
+
+import javax.naming.ldap.PagedResultsControl;
+import javax.print.attribute.standard.PageRanges;
+
+
+import org.sid.dao.ProduitRepository;
+import org.sid.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProduitController {
+	@Autowired
+	private ProduitRepository produitRepository;
+	
+	@RequestMapping(value="/index2")
+	public String index(Model model,@RequestParam(name="page",defaultValue = "0")int p,
+			@RequestParam(name="size",defaultValue = "5")int s, @RequestParam(name="mc", defaultValue="")String mc) {
+		Page<Produit> pageproduits = produitRepository.ProduitParMC("%"+mc+"%",PageRequest.of(p, s));
+		model.addAttribute("listProduits",pageproduits.getContent() );
+		int[] pagenum = new int[pageproduits.getTotalPages()];
+		model.addAttribute("pages",pagenum);
+		model.addAttribute("currentPage",p);
+		model.addAttribute("size",s);
+		model.addAttribute("mc",mc);
+		return "produits";
+	}
+	@RequestMapping(value="/delete",method = RequestMethod.GET)
+	public String delete(Long id,@RequestParam(name="page",defaultValue = "0")int p,
+			@RequestParam(name="size",defaultValue = "5")int s, @RequestParam(name="mc", defaultValue="")String mc) {
+			produitRepository.deleteById(id);
+		return "redirect:/index2?page="+p+"&size="+s+"&mc="+mc;
+	}
+}
